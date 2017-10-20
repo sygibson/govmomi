97ee177ebeed86623b5884d5a0f5b4861dca0f1e
Doug MacEachern <dougm@vmware.com>
2017-10-20 13:19:36 -0700

    Update to vmodl version 12
    Provides early access to upcoming 6.7 APIs.
    
    In the generator, we can no longer ignore all types that start with a lower-case letter as
    'vslmInfrastructureObjectPolicy' is a field type in other structures.

--- gen/gen_from_vmodl.rb
+++ gen/gen_from_vmodl.rb
@@ -85,7 +85,6 @@ class Prop
         type ="int64"
       when "dateTime"
         type ="time.Time"
-        prefix += "*" if !slice? && optional?
       when "byte"
       when "double"
         type ="float64"
@@ -152,8 +151,11 @@ class Managed
       end
     end
 
+    seen = {}
     props.each do |p|
+      next if seen[p.name]
       p.dump(io)
+      seen[p.name] = true
     end
     io.print "}\n\n"
 
